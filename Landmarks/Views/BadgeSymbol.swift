3d5694675034ef507c1f190c2895a590acfeb242
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-09-23 23:10:43 -0700

    Created the badge design and app icon and badge symbol

---
+++ Landmarks/Views/BadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Ikbal Demirdoven on 2024-09-23.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeSymbol()
+}
