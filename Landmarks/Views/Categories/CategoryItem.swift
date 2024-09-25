14ac8a63b6b70db779b8cb60a621e24ea265c0e2
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-09-24 23:03:05 -0700

    added categories

---
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Ikbal Demirdoven on 2024-09-24.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Ikbal Demirdoven on 2024-09-24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
