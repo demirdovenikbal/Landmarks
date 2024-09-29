241265991839561d3c7f0e8d1034c7045c6cbecb
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-09-28 19:58:37 -0700

    Completed and implemented the featured carousel at the top of the main view

---
+++ Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,18 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Ikbal Demirdoven on 2024-09-28.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageView()
+}
---
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Ikbal Demirdoven on 2024-09-28.
+//
+
+import Foundation
