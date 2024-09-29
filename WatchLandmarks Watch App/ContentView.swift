ee4031bd1c2b8f6b2de9384da108b5977cef3dba
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-09-28 20:34:26 -0700

    created the watchos Landmark app

---
+++ WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Ikbal Demirdoven on 2024-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
