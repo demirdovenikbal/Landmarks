d253a738cd3ca9f7d08113cc4cc45bc128423274
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-09-30 01:05:07 -0700

    Implemented the notifications and completed the watchOS app.

---
+++ WatchLandmarks Watch App/NotificationController.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationController.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Ikbal Demirdoven on 2024-09-30.
+//
+
+import Foundation
---
+++ WatchLandmarks Watch App/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Ikbal Demirdoven on 2024-09-30.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
