4ac0782505cb3f6e59da48089352e168258d28f5
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-09-29 23:42:16 -0700

    some changes in the apple watch app UI

---
+++ WatchLandmarks Watch App/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Ikbal Demirdoven on 2024-09-28.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
