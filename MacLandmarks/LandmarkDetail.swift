76659a702141060aefb722a07c87e977b5a1487d
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-10-01 22:07:44 -0700

    Implemented the LandmarkDetail view, implemented the MapKit to be able to open the location in Apple Maps

---
+++ MacLandmarks/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  MacLandmarks
+//
+//  Created by Ikbal Demirdoven on 2024-09-30.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
