5c122200e67fb9d2e0dd26277ca225975c60e73f
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-10-01 22:30:45 -0700

    Made appropriate UI changes for different platforms like iOS, macOS and watchOS

---
+++ WatchLandmarks Watch App/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Ikbal Demirdoven on 2024-10-01.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
