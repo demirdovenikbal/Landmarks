2295f33aa937c71ca56c61f8f3b2c8357e4a00ff
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-10-03 23:08:25 -0700

    Finalized the app.

---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by Ikbal Demirdoven on 2024-10-03.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkSettings()
+}
